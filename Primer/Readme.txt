c3d4874c2f01118b04091f8929f426b13386fff4
game101 <nienieshou@gmail.com>
2019-04-26 15:27:03 +0800

    character

--- Primer/Readme.txt
+++ Primer/Readme.txt
@@ -1,20 +1,26 @@
 unreal4.20.3 
 C++ Project
 
-备份部分
-Primer.uproject
-Source
-Content\Blueprints
+==================================
+
+Backup part
+1Primer.uproject
+2Source
+3Content\Blueprints
+
+==================================
 
 git init
-git add .
-git commit -m "ultimate"
-git remote add origin https://github.com/game102/UE4Articles.git
-git push -u origin master
 
+==================================
 touch .gitignore
+
 /*
-!.gitignore
+
+!/Primer/
+
+/Primer/*
+!/Primer/Readme.txt
 !/Primer/Primer.uproject
 !/Primer/Source
 !/Primer/Content/PrimerActor
@@ -23,3 +29,9 @@ touch .gitignore
 !/Primer/Content/PrimerLevel
 !/Primer/Content/PrimerMechanics
 !/PrimerContent/PrimerPawn
+==================================
+
+git add .
+git commit -m "ultimate"
+git remote add origin https://github.com/game102/UE4Articles.git
+git push -u origin master
---
+++ Primer/Source/Primer/MainCharacter.cpp
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine//World.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFrameWork/CharacterMovementComponent.h"
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create Camera Boom, pulls twords the player if there's a collision.
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 600.f;        //Camera follows at this distance
+	CameraBoom->bUsePawnControlRotation = true;  //Rotate arm based on controller
+
+	//Set size for collision capsule
+	GetCapsuleComponent()->SetCapsuleSize(48.f, 105.f);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	//A socket is something that can be put onto an asset such as a skeleton and it allows you attach thing to it
+	//put a socket on the hand bone to attach sword to it
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+	//Set our turn rates for input
+	BaseTurnRate = 65.f;
+	BaseLookUpRate = 65.f;
+
+	//Don't rotate when the controller rotates Let that just affect the camera
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	//Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;  //Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.f, 0.0f); //... at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 650.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	check(PlayerInputComponent);  //stop the code  here unless palyer input component is valid
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMainCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMainCharacter::StopJumping);
+
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis("Turn", this, &AMainCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AMainCharacter::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMainCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AMainCharacter::LookUpAtRate);
+}
+
+void AMainCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		//find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+		//X direction as sort of like the forward direction Y would be like the right direction and X would be like up
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		//find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+		//X direction as sort of like the forward direction Y would be like the right direction and X would be like up
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainCharacter::TurnAtRate(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds()); 
+}
+
+void AMainCharacter::LookUpAtRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
